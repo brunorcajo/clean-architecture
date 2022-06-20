bf5c0356696d090f40fd5f468c0f96e6d1044986
Bruno Orcajo <bruno@orcajo.com.br>
2022-06-20 08:49:17 -0300

    test: should add user with complete data to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  it('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingListUseCase(repo)
+    // const name: string = 'any_name'
+    // const email: string = 'any@email.com'
+    // const response = await usecase.RegisterUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
